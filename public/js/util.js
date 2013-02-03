eae6d7c79c7b4aa89fab75694a44db868fe4765e
Qian Long <hungryha@gmail.com>
2013-02-03 03:36:38 -0500

    added qr code for room assignments UNTESTED lol

--- public/js/util.js
+++ public/js/util.js
@@ -7,5 +7,6 @@ function util_generate_qr_code(encoded_url) {
   var root_url = "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=" 
   var api_url = root_url + encoded_url;
   var img = $('<img src=\'' + api_url + '\' />');
-  img.appendTo("#qr_code");
+  //img.appendTo("#qr_code");
+  return img;
 }
