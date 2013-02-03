7bd1641a0cdc08d4af11bbb53ee3cac041612435
Stephanie Yu <styu314@gmail.com>
2013-02-03 08:45:34 -0500

    Added two more players

--- public/js/pong.js
+++ public/js/pong.js
@@ -1,32 +1,53 @@
-function calculateAngle(player, ball) {
+function calculateAngleY(player, ball) {
   return 2 * ((ball.ball.y - player.paddle.y) / 25.0);
 }
 
+function calculateAngleX(player, ball) {
+  return 2 * ((ball.ball.x - player.paddle.x) / 25.0);
+}
 /*
  * Game class
  */
-function Game(player1, player2, ball) {
+function Game(player1, player2, player3, player4, ball) {
   this.player1 = player1;
   this.player2 = player2;
+  this.player3 = player3;
+  this.player4 = player4;
   this.ball = ball;
 }
 
-Game.prototype.makeMove = function(y1, y2) {
-  this.player1.movePaddle(y1);
-  this.player2.movePaddle(y2);
+Game.prototype.makeMove = function(y1, y2, x3, x4) {
+  this.player1.movePaddleY(y1);
+  this.player2.movePaddleY(y2);
+  this.player3.movePaddleX(x3);
+  this.player4.movePaddleX(x4);
   this.ball.moveBall();
 }
 
-Game.prototype.checkForCollision = function() {
-  if (this.player1.isTouching(this.ball)) {
+Game.prototype.checkForCollisionY = function() {
+  if (this.player1.isTouchingY(this.ball)) {
     if (this.ball.speedX < 0) {
       this.ball.speedX *= -1;
-      this.ball.speedY = calculateAngle(this.player1, this.ball);
+      this.ball.speedY = calculateAngleY(this.player1, this.ball);
     }
-  } else if (this.player2.isTouching(this.ball)) {
+  } else if (this.player2.isTouchingY(this.ball)) {
     if (this.ball.speedX > 0) {
       this.ball.speedX *= -1;
-      this.ball.speedY = calculateAngle(this.player2, this.ball);
+      this.ball.speedY = calculateAngleY(this.player2, this.ball);
+    }
+  }
+}
+
+Game.prototype.checkForCollisionX = function() {
+  if (this.player3.isTouchingX(this.ball)) {
+    if (this.ball.speedY < 0) {
+      this.ball.speedY *= -1;
+      this.ball.speedX = calculateAngleX(this.player3, this.ball);
+    }
+  } else if (this.player4.isTouchingX(this.ball)) {
+    if (this.ball.speedY > 0) {
+      this.ball.speedY *= -1;
+      this.ball.speedX = calculateAngleX(this.player4, this.ball);
     }
   }
 }
@@ -36,8 +57,6 @@ Game.prototype.checkForCollision = function() {
  */
 function Player(paddle, element) {
   this.paddle = paddle;
-  this.middleY;
-  this.calibrated = false;
   this.score = 0;
   this.element = element;
   this.height = element.style.height;
@@ -45,20 +64,10 @@ function Player(paddle, element) {
 }
 
 /*
- * Sets the first position of the position as the 'middle'
- * of the pong game, such that all movements are relative
- * from the phone position
- */
-Player.prototype.calibrate = function(yCoord) { 
-  this.middleY = yCoord;
-  this.calibrated = true;
-};
-
-/*
  * Moves the paddle by setting the y coordinate, calibrated
  * beforehand
  */
-Player.prototype.movePaddle = function(yCoord) {
+Player.prototype.movePaddleY = function(yCoord) {
   if (yCoord <= 0) {
     this.paddle.y = 0;
   } else if (yCoord >= $(document).height() - 120) {
@@ -70,15 +79,45 @@ Player.prototype.movePaddle = function(yCoord) {
 };
 
 /*
+ * Moves the paddle by setting the y coordinate, calibrated
+ * beforehand
+ */
+Player.prototype.movePaddleX = function(xCoord) {
+  if (xCoord <= 0) {
+    this.paddle.x = 0;
+  } else if (xCoord >= $(document).width() - 120) {
+    this.paddle.x = $(document).width() - this.width;
+  } else {
+    this.paddle.x = xCoord;
+  }
+};
+
+/*
  * Returns true if the given object is touching the player
  */
-Player.prototype.isTouching = function(pingpongball) {
+Player.prototype.isTouchingY = function(pingpongball) {
   if (pingpongball.ball.x <= pingpongball.radius - $(window).width() / 2 + 65){
     if (pingpongball.ball.y > this.paddle.y && pingpongball.ball.y < (this.paddle.y + 120)){
       return true;
     }
   } else if (pingpongball.ball.x >= $(window).width() / 2 - pingpongball.radius - 65) {
     if (pingpongball.ball.y > this.paddle.y && pingpongball.ball.y < (this.paddle.y + 120)){
+      return true;
+    }
+  }
+  return false;
+};
+
+/*
+ * Returns true if the given object is touching the player
+ */
+Player.prototype.isTouchingX = function(pingpongball) {
+  if (pingpongball.ball.y <= pingpongball.radius - $(window).height() / 2 + 65){
+    if (pingpongball.ball.x > this.paddle.x && pingpongball.ball.x < (this.paddle.x + 120)){
+      return true;
+    }
+  } else if (pingpongball.ball.y >= $(window).height() / 2 - pingpongball.radius - 65) {
+    if (pingpongball.ball.x > this.paddle.x && pingpongball.ball.x < (this.paddle.x + 120)){
       return true;
     }
   }
@@ -133,8 +172,8 @@ var socket = io.connect(window.location.origin);
 var stage;
 var h = $(window).height(),
     w = $(window).width();
-var y1 = 0, y2 = 0,
-    player1, player2,
+var y1 = 0, y2 = 0, y3 = 0, y4 = 0,
+    player1, player2, player3, player4,
     pingpongball,
     game;
 
@@ -148,17 +187,13 @@ var y1 = 0, y2 = 0,
 socket.on('controls', function (data) {
   //console.log(data);
   if (data["playerNumber"] === 1) {
-    //y1 = data["agY"];
     y1 = h * ((data["agY"] + 5) / 10);
-    if (!player1.calibrated) {
-      player1.calibrate(y1);
-    }
   } else if (data["playerNumber"] === 2) {
-    //y2 = data["agY"];
     y2 = h * ((data["agY"] + 5) / 10);
-    if (!player2.calibrated) {
-      player2.calibrate(y2);
-    }
+  } else if (data["playerNumber"] === 3) {
+    y3 = w * ((data["agX"] + 5) / 10);
+  } else if (data["playerNumber"] === 4) {
+    y4 = w * ((data["agX"] + 5) / 10);
   }
 });
 
@@ -186,19 +221,27 @@ function init()
 
   var paddle1 = document.getElementById("player1"),
       paddle2 = document.getElementById("player2"),
+      paddle3 = document.getElementById("player3"),
+      paddle4 = document.getElementById("player4"),
       ball = document.getElementById("ball"),
       paddle1DOMElement = new createjs.DOMElement(paddle1),
       paddle2DOMElement = new createjs.DOMElement(paddle2),
+      paddle3DOMElement = new createjs.DOMElement(paddle3),
+      paddle4DOMElement = new createjs.DOMElement(paddle4),
       ballDOMElement = new createjs.DOMElement(ball);
 
 	player1 = new Player(paddle1DOMElement, paddle1);
 	player2 = new Player(paddle2DOMElement, paddle2);
+	player3 = new Player(paddle3DOMElement, paddle3);
+	player4 = new Player(paddle4DOMElement, paddle4);
 	pingpongball = new Ball(ballDOMElement, ball);
   game = new Game(player1, player2, pingpongball);
 
 	//add the paddles to the stage.
   stage.addChild(paddle1DOMElement);
   stage.addChild(paddle2DOMElement);
+  stage.addChild(paddle3DOMElement);
+  stage.addChild(paddle4DOMElement);
   stage.addChild(ballDOMElement);
 
 	//tell the stage to render to the canvas
@@ -215,7 +258,8 @@ function init()
 //function called by the Tick instance at a set interval
 function tick()
 {
-  game.makeMove(y1, y2);
-  game.checkForCollision();
+  game.makeMove(y1, y2, y3, y4);
+  game.checkForCollisionY();
+  game.checkForCollisionX();
 	stage.update();
 }
