a2cbaa58cbea9b794bdd08e355d7f9508b3a26c1
poofytoo <victorhung92@gmail.com>
2013-02-03 02:46:58 -0500

    two paddles

--- public/js/pong.js
+++ public/js/pong.js
@@ -13,8 +13,9 @@ Player.prototype.calibrate = function(yCoord) {
   this.calibrated = true;
 };
 
-Player.prototype.movePaddle = function(yCoord) {
-  this.paddle.y = yCoord;
+Player.prototype.movePaddle = function(yCoord1,yCoord2) {
+  this.paddle1.y = yCoord1;
+  this.paddle2.y = yCoord2;
 }
 
 
@@ -23,7 +24,11 @@ var y = 0;
 
 socket.on('controls', function (data) {
   console.log(data);
-  y = data["mouseY"];
+  if (data["playerNumber"] == "1"){
+	y1 = data["mouseY"];
+  } else if (data["playerNumber"] == "2"){
+	y2 = data["mouseY"];
+  }
 });
 //EaselJS Stage instance that wraps the Canvas element
 var stage;
@@ -65,12 +70,14 @@ function init()
 	//is the root level display container for display elements.
 	stage = new createjs.Stage(canvas);
 
-  var paddle = document.getElementById("player1");
-  var paddleDOMElement = new createjs.DOMElement(paddle);
-	player = new Player(paddleDOMElement);
+  var paddle1 = document.getElementById("player1");
+  var paddle2 = document.getElementById("player2");
+  var paddle1DOMElement = new createjs.DOMElement(paddle1);
+  var paddle2DOMElement = new createjs.DOMElement(paddle2);
+	player = new Player(paddle1DOMElement);
 
 	//add the paddle to the stage.
-  stage.addChild(paddleDOMElement);
+  stage.addChild(paddle1DOMElement);
 
 	//tell the stage to render to the canvas
 	stage.update();
@@ -86,6 +93,6 @@ function init()
 //function called by the Tick instance at a set interval
 function tick()
 {
-  player.movePaddle(y);
+  player.movePaddle(y1,y2);
 	stage.update();
 }
